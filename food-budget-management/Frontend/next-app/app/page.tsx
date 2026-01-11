a21069f3b51d96cbd28ea9ed2b3f09759fac8f08
Trung Duc Pham <tduc@my.yorku.ca>
2026-01-10 22:18:25 -0500

    Change header layout, add sideBar

--- food-budget-management/Frontend/next-app/app/page.tsx
+++ food-budget-management/Frontend/next-app/app/page.tsx
@@ -1,5 +1,17 @@
+"use client"
+import Header from "./components/Header";
+import SideNavBar from "./components/SideNavBar";
+import { useState } from "react";
 export default function Home() {
+  const [sideBarOpen, setSideBarState] = useState(false);
+  console.log(sideBarOpen);
   return (
-   <p>Hello</p>
+  <>
+   <Header sideBarHandler={() => setSideBarState(!sideBarOpen)}></Header>
+   <div className={`transform transition-transform duration-500 ${sideBarOpen ? "translate-x-0" : "-translate-x-full"}`}>
+     <SideNavBar/>
+   </div>
+   
+  </>
   );
 }
