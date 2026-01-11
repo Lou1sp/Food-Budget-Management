209b4644c24df8e9b6da0a05c0ff154577a79528
Louis Pham <duchp1104@gmail.com>
2026-01-10 22:18:25 -0500

    Change header layout, add sideBar

---
+++ food-budget-management/Frontend/next-app/app/components/Header.tsx
@@ -0,0 +1,20 @@
+'use client'
+import {Menu, X} from "lucide-react";
+import Link from "next/link";
+interface HeaderProps{
+    sideBarHandler: () => void;
+}
+export default function Header({sideBarHandler}: HeaderProps){
+    return(
+        <div className="w-full bg-blue-600 h-17 flex items-center justify-between">
+             <div className="flex ml-5">
+                <button className="cursor-pointer " onClick={sideBarHandler}>
+                   <div className="border border-white rounded-md p-1">
+                    <Menu size={30}></Menu>
+                   </div> 
+                </button>
+                <p className="p-7 font-mono font-bold text-2xl">Company Logo</p>
+             </div>
+        </div>
+    );
+}
---
+++ food-budget-management/Frontend/next-app/app/components/SideNavBar.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+import profilePic from "../../public/profile_picture.jpg"
+export default function SideNavBar() {
+  return (
+    <aside className="fixed top-0 left-0 w-64 bg-white shadow-lg z-50 h-[calc(100vh-4.25rem)]">
+      <div className="mt-5 mb-8 ml-5 flex items-center gap-3">
+        <Image src={profilePic} alt="" width={70} height={50}></Image>
+        <div className="text-gray-700 font-mono font-light">
+           <i> 
+            <p>User Name</p> 
+            <p>ID:11042005</p>
+           </i>
+            
+        </div>
+      </div>
+
+      <nav className="flex flex-col gap-4 font-mono ml-5 mr-5">
+        <a
+          href="#activity"
+          className="text-gray-700 hover:bg-blue-100 hover:text-blue-700 px-3 py-2 rounded transition-colors duration-200"
+        >
+          Activity
+        </a>
+        <a
+          href="#budget"
+          className="text-gray-700 hover:bg-blue-100 hover:text-blue-700 px-3 py-2 rounded transition-colors duration-200"
+        >
+          Budget
+        </a>
+        <a
+          href="#records"
+          className="text-gray-700 hover:bg-blue-100 hover:text-blue-700 px-3 py-2 rounded transition-colors duration-200"
+        >
+          Records
+        </a>
+        <a
+          href="#market"
+          className="text-gray-700 hover:bg-blue-100 hover:text-blue-700 px-3 py-2 rounded transition-colors duration-200"
+        >
+          Market
+        </a>
+      </nav>
+
+      <hr className="text-black mx-auto w-50 mt-20"></hr>
+
+      <nav className="flex flex-col gap-4 font-mono ml-5 mr-5 mt-5 font-extralight">
+        <a
+          href="#market"
+          className="text-gray-700 hover:bg-blue-100 hover:text-blue-700 px-3 py-2 rounded transition-colors duration-200"
+        >
+          About Us
+        </a>
+        <a
+          href="#market"
+          className="text-gray-700 hover:bg-blue-100 hover:text-blue-700 px-3 py-2 rounded transition-colors duration-200"
+        >
+          Contact
+        </a>
+      </nav>
+
+      {/* Footer optional */}
+      <div className="absolute bottom-2 ml-2 text-gray-500 text-sm">
+        © 2026 MyApp
+      </div>
+    </aside>
+  );
+}
